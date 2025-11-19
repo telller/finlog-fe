95b35d5fa187209f9f9764cd8c8fb35869d0c909
Vladyslav Holiachenko <vlad.golyachenko@gmail.com>
2025-11-19 20:59:05 +0200

    wip

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,9 +1,9 @@
-import type { CSSProperties } from 'react';
+import { MenuOutlined } from '@ant-design/icons';
+import { type CSSProperties, useState } from 'react';
 import { Outlet } from 'react-router';
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 
 const headerStyle: CSSProperties = {
-  textAlign: 'center',
   color: '#fff',
   height: 64,
   paddingInline: 48,
@@ -19,21 +19,33 @@ const contentStyle: CSSProperties = {
   backgroundColor: '#0958d9',
 };
 
-// const siderStyle: CSSProperties = {
-//     textAlign: 'center',
-//     lineHeight: '120px',
-//     color: '#fff',
-//     backgroundColor: '#1677ff',
-// };
-
 const MainLayout = () => {
+  const [isOpenedSidebar, $isOpenedSidebar] = useState(false);
+  const siderStyle: CSSProperties = {
+    position: 'fixed',
+    height: '100%',
+    textAlign: 'center',
+    lineHeight: '120px',
+    width: '160px',
+    color: '#fff',
+    backgroundColor: '#1677ff',
+    transform: isOpenedSidebar ? 'translateX(0)' : 'translateX(-160px)',
+    transition: 'transform 0.2s ease',
+    zIndex: 1000,
+  };
   return (
     <Layout>
-      <Layout.Header style={headerStyle}>Header</Layout.Header>
+      <Layout.Header style={headerStyle}>
+        <Button
+          onClick={() => $isOpenedSidebar(!isOpenedSidebar)}
+          icon={<MenuOutlined />}
+          shape="circle"
+        />
+      </Layout.Header>
       <Layout>
-        {/*<Sider width="25%" style={siderStyle}>*/}
-        {/*    Sider*/}
-        {/*</Sider>*/}
+        <Layout.Sider width={160} style={siderStyle}>
+          Sider
+        </Layout.Sider>
         <Layout.Content style={contentStyle}>
           <Outlet />
         </Layout.Content>
